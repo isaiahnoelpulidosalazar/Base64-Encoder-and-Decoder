7d89b22d6d500bdc63be85d9145a1fbc95eaffc0
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-06-16 11:25:04 +0800

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Base64_Encoder_and_Decoder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            comboBox2 = new ComboBox();
+            EnterButton = new Button();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.ImeMode = ImeMode.Off;
+            comboBox1.ItemHeight = 15;
+            comboBox1.Items.AddRange(new object[] { "Encode", "Decode" });
+            comboBox1.Location = new Point(14, 14);
+            comboBox1.Margin = new Padding(4, 3, 4, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(140, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(14, 45);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(437, 449);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(460, 17);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Result";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Window;
+            textBox2.Location = new Point(463, 45);
+            textBox2.Margin = new Padding(4, 3, 4, 3);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(437, 449);
+            textBox2.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.ImeMode = ImeMode.Off;
+            comboBox2.Items.AddRange(new object[] { "UTF-8", "UTF-32" });
+            comboBox2.Location = new Point(162, 14);
+            comboBox2.Margin = new Padding(4, 3, 4, 3);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(140, 23);
+            comboBox2.TabIndex = 4;
+            // 
+            // EnterButton
+            // 
+            EnterButton.Location = new Point(310, 14);
+            EnterButton.Margin = new Padding(4, 3, 4, 3);
+            EnterButton.Name = "EnterButton";
+            EnterButton.Size = new Size(88, 24);
+            EnterButton.TabIndex = 5;
+            EnterButton.Text = "Enter";
+            EnterButton.UseVisualStyleBackColor = true;
+            EnterButton.Click += EnterButton_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(915, 509);
+            Controls.Add(EnterButton);
+            Controls.Add(comboBox2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Base64";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button EnterButton;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,67 @@
+using System.Text;
+
+namespace Base64_Encoder_and_Decoder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text.Equals("Encode"))
+                {
+                    try
+                    {
+                        if (comboBox2.Text.Equals("UTF-8"))
+                        {
+                            var a = Encoding.UTF8.GetBytes(textBox1.Text);
+                            var b = Convert.ToBase64String(a);
+                            textBox2.Text = b;
+                        }
+                        else if (comboBox2.Text.Equals("UTF-32"))
+                        {
+                            var a = Encoding.UTF32.GetBytes(textBox1.Text);
+                            var b = Convert.ToBase64String(a);
+                            textBox2.Text = b;
+                        }
+                    }
+                    catch
+                    {
+                        textBox2.Text = "Something went wrong while encoding the text.";
+                    }
+                }
+                else if (comboBox1.Text.Equals("Decode"))
+                {
+                    try
+                    {
+                        if (comboBox2.Text.Equals("UTF-8"))
+                        {
+                            var a = Convert.FromBase64String(textBox1.Text);
+                            var b = Encoding.UTF8.GetString(a);
+                            textBox2.Text = b;
+                        }
+                        else if (comboBox2.Text.Equals("UTF-32"))
+                        {
+                            var a = Convert.FromBase64String(textBox1.Text);
+                            var b = Encoding.UTF32.GetString(a);
+                            textBox2.Text = b;
+                        }
+                    }
+                    catch
+                    {
+                        textBox2.Text = "Something went wrong while decoding the text.";
+                    }
+                }
+            }
+            catch
+            {
+                textBox2.Text = "Please select a format.";
+            }
+        }
+    }
+}
